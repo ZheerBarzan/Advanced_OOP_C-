c4db2023ce101fecf474966f54bf6e0307a218e5
zheer barzan <zheerbarzan85@outlook.com>
2024-01-13 23:12:37 +0300

    finsihing the triangle problem

---
+++ Lab_1/Question_1.cpp
@@ -0,0 +1,20 @@
+//
+// Created by zheer barzan on 1/13/24.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height;
+    int base;
+
+    cout << "enter the hight: " << endl ;
+    cin>> height;
+    cout<< "enter the base : "<<endl;
+    cin>>base;
+
+    int result = (height*base)/2;
+
+    cout<< "the Area of the triangle is: "<< result;
+
+}
---
+++ cmake-build-debug/CMakeFiles/Advanced_OOP_C__.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Advanced_OOP_C__"
+  "Advanced_OOP_C__.pdb"
+  "CMakeFiles/Advanced_OOP_C__.dir/Lab_1/Question_1.cpp.o"
+  "CMakeFiles/Advanced_OOP_C__.dir/Lab_1/Question_1.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Advanced_OOP_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
